4d24604e8e8b52d2af1d688dc925eb37f7de8ea2
James Socol <james@mozilla.com>
2012-12-12 14:42:47 -0500

    Dragging things around works.

--- moodboard.js
+++ moodboard.js
@@ -4,8 +4,16 @@ function p(e) {
     e.preventDefault();
 }
 
+var dragCurrent = false;
 function dragStart(e) {
     p(e);
+    var obj = e.target;
+    while(obj.parentNode && obj.nodeName.toLowerCase() != 'div') {
+        obj = obj.parentNode;
+    }
+    dragCurrent = obj;
+    var offset = {'x': e.layerX, 'y': e.layerY};
+    dragCurrent.offset = offset;
 }
 
 function dragEnter(e) {
@@ -20,18 +28,37 @@ function dragDrop(e) {
     var x = e.pageX;
     var y = e.pageY;
     var dropped = document.createElement('div');
+    var dt = e.dataTransfer;
     dropped.style.left = x + 'px';
     dropped.style.top = y + 'px';
-    var text = e.dataTransfer.getData('text/plain');
+    dropped.draggable = true;
+    var text = dt.getData('text/plain');
     if (imgRegex.test(text)) {
         var img = new Image();
         img.src = text;
         img.addEventListener('load', function() {
             dropped.style.height = this.height + 'px';
             dropped.style.width = this.width + 'px';
-            console.log(this, dropped);
         }, false);
         dropped.appendChild(img);
+    }
+    else if (dt.files.length) {
+        for (var i = 0; i < dt.files.length; i++) {
+            var f = dt.files[i];
+            if (!f.type.match(/image.*/)) {
+                continue;
+            }
+            var img = new Image();
+            img.file = f;
+            dropped.appendChild(img);
+            var reader = new FileReader();
+            reader.onload = (function(aImg) {
+                return function(e) {
+                    aImg.src = e.target.result;
+                };
+            })(img);
+            reader.readAsDataURL(f);
+        }
     }
     else {
         dropped.textContent = text;
@@ -39,9 +66,26 @@ function dragDrop(e) {
     board.appendChild(dropped);
 }
 
+function mouseMove(e) {
+    p(e);
+    if (!dragCurrent) return;
+    var s = {
+        'x': e.pageX - dragCurrent.offset.x,
+        'y': e.pageY - dragCurrent.offset.y
+    };
+    dragCurrent.style.left = s.x + 'px';
+    dragCurrent.style.top = s.y + 'px';
+}
+
+function mouseUp(e) {
+    if (dragCurrent)
+        dragCurrent = false;
+}
+
 function dragEnd(e) {
     p(e);
     msg.style.display = 'none';
+    dragCurrent = false;
 }
 
 function $(sel) {
@@ -54,6 +98,8 @@ board.addEventListener('dragstart', dragStart, false);
 board.addEventListener('dragenter', dragEnter, false);
 board.addEventListener('dragover', p, false);
 board.addEventListener('drop', dragDrop, false);
+board.addEventListener('mousemove', mouseMove, false);
+board.addEventListener('mouseup', mouseUp, false);
 
 var msg = $('#dropMessage')[0];
 
